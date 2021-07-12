88a25c77cb7d5a7ecb7fc97b3be28a666575463e
priyanshu-rgb <priyanshumandal2001@gmail.com>
2021-07-13 00:14:56 +0530

    jj

--- Django_projects/jj/travel/admin.py
+++ Django_projects/jj/travel/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . models import user,travel
 # Register your models here.
+
+admin.site.register(user)
+admin.site.register(travel)
--- Django_projects/jj/travel/views.py
+++ Django_projects/jj/travel/views.py
@@ -1,8 +1,31 @@
+
 from django.http.response import HttpResponse
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .models import user
 
 # Create your views here.
 
 def home(request):
-    return render(request,'signup.html')
+    return render(request,'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        phno = request.POST['phone']
+        email = request.POST['email']
+        username= request.POST['username']
+        pass1 = request.POST['psw1']
+        pass2 = request.POST['psw2']
+
+        User= user.objects.create_user(name=name, phno=phno, email=email, password=pass1, username=username) 
+        
+        User.save();
+        print('user created')
+        return redirect('')
+
+
+    else:
+        return render(request,'signup.html')
+
+def enter(request):
+    return render(request,'signin.html')
